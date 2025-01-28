0e84f44dbdc68c5939112b0c789b48f410109828
Seth Adams <splatterscorlos@gmail.com>
2025-01-28 15:59:10 -0700

    Greeter project

---
+++ InteractiveGreeter-SethAdams.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ");
+origin = input("Where are you from? ");
+activity = input("What's something you like to do? ");
+print("Hello " + name + " from " + origin + ", have fun when you " + activity + ".");
+exit();
