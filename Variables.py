dcec2303d08403c0151bc627caa212190e774fe5
Seth Adams <splatterscorlos@gmail.com>
2025-01-29 15:18:23 -0700

    Variables practice

---
+++ Variables.py
@@ -0,0 +1,12 @@
+# Learning Variables
+lastName = "Adams"
+firstName = "Seth"
+temp = 50
+
+#print(type(lastName));
+#print(type(firstName));
+#print(type(temp));
+#print(temp);
+#print(temp + 2);
+
+print("Hello my name is " + firstName + " " + lastName + ", today's temperatue is " + str(temp) + " degrees.");
